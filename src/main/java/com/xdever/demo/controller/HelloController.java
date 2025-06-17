0bb2800397cd619cf92615f38003900018ccadd9
398481657@qq.com <398481657@qq.com>
2025-06-18 00:26:56 +0800

    init

---
+++ src/main/java/com/xdever/demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.xdever.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+
+    @GetMapping("/say")
+    public String say() {
+        return "Hello.";
+    }
+
+}
