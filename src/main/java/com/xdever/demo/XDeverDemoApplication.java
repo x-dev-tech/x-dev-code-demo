3833ac7b77834b46b93ad500f67a2cfbb67a039d
X Dev Boy <398481657@qq.com>
2025-06-18 00:42:01 +0800

    init (#1)

---
+++ src/main/java/com/xdever/demo/XDeverDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xdever.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XDeverDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XDeverDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xdever/demo/XdeverDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xdever.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XdeverDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
